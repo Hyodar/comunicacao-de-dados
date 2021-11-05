cf5294210092c276eb9ab12bf59539e3aea09e13
Hyodar <franco.b.gomes@gmail.com>
2021-11-05 01:47:23 -0300

    Add cryptography placeholder

---
+++ src/utils/cryptography.ts
@@ -0,0 +1,14 @@
+
+export default class Cryptography {
+    static encrypt(text: Buffer): Buffer {
+        // TODO
+
+        return text;
+    }
+
+    static decrypt(text: Buffer): Buffer {
+        // TODO
+
+        return text;
+    }
+}
