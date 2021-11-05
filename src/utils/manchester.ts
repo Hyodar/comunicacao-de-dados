cd6e097a9749d7321fbecda4dac0c8e98e507323
Hyodar <franco.b.gomes@gmail.com>
2021-11-05 01:47:04 -0300

    Export manchester encoding functions as a class

--- src/utils/manchester.ts
+++ src/utils/manchester.ts
@@ -1,7 +1,10 @@
-export function encode(msg: string): string {
-  return msg.split("").map(bit => (bit === "1") ? "01" : "10").join("");
-}
 
-export function decode(msg: string): string {
-  return (msg.match(/..?/g) || []).map(bit => (bit === "01")? "1" : "0").join("");
+export default class ManchesterEncoding {
+  static encode(msg: string): string {
+    return msg.split("").map(bit => (bit === "1") ? "01" : "10").join("");
+  }
+
+  static decode(msg: string): string {
+    return (msg.match(/..?/g) || []).map(bit => (bit === "01")? "1" : "0").join("");
+  }
 }
